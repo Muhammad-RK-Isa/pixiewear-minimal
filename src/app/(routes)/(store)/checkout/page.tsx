ae3acdf7df07e8531a7a7957659178597dabfc26
muhammad-rk-isa <muhammad-rk-isa@outlook.com>
2025-01-25 01:43:33 +0600

    fix: checkout pre-rendering error

--- src/app/(routes)/(store)/checkout/page.tsx
+++ src/app/(routes)/(store)/checkout/page.tsx
@@ -5,6 +5,8 @@ import type { Metadata } from "next";
 import { env } from "~/env";
 import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+
 export const metadata: Metadata = {
   metadataBase: new URL(env.NEXT_PUBLIC_APP_URL),
   title: "Checkout",
